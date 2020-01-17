3a0a7e4ff5d7c406d4bc8bb1d2d7a6409035cf15
John Karahalis <john.karahalis@gmail.com>
2020-01-16 19:34:32 -0500

    Issue #1: Remove unused "os" import

--- src/redash_iodide/groups.py
+++ src/redash_iodide/groups.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 import logging
-import os
 
 from redash.handlers.base import BaseResource, get_object_or_404
 from redash.models import Query
